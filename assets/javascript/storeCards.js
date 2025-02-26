a6139b7efc771e61dd7748a57f9de2e9b9bccca9
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2025-02-27 00:27:47 +0500

    Mobile view completed

--- assets/javascript/storeCards.js
+++ assets/javascript/storeCards.js
@@ -1,9 +1,9 @@
 let mainStoreContainer = document.querySelector('.storeMainContent');
 let characters = [
     {
-        img: 'https://pm1.aminoapps.com/7652/322a7bb59aa028fdb37d32172158281679c2d0a5r1-720-720v2_00.jpg',
+        img: 'https://img.freepik.com/premium-photo/character-with-sword-helmet-with-word-war-it_849715-1443.jpg',
         skinName: 'Galatic Clash',
-        'characterName': 'Senna William'
+        'characterName': 'Commander Joseph'
 
     },
     {
